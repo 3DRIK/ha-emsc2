4ee3a747ece10b44f017f13623f15c7a6226d89b
3DRIK <96953423+3DRIK@users.noreply.github.com>
2026-03-11 14:58:50 +0100

    Create const.py

---
+++ custom_components/emsc/const.py
@@ -0,0 +1,18 @@
+"""Constants for the Earthquake Monitoring integration."""
+DOMAIN = "zemetrasenia"
+
+CONF_MIN_MAGNITUDE = "min_magnitude"
+CONF_RADIUS = "radius"
+
+DEFAULT_MIN_MAGNITUDE = 3.0
+DEFAULT_RADIUS = 500.0  # km
+
+WS_URI = "wss://www.seismicportal.eu/standing_order/websocket"
+
+EVENT_EARTHQUAKE_DETECTED = "zemetrasenia.earthquake_detected"
+
+ATTR_MAGNITUDE = "magnitude"
+ATTR_LATITUDE = "latitude"
+ATTR_LONGITUDE = "longitude"
+ATTR_LOCALITY = "locality"
+ATTR_TIME = "time"
